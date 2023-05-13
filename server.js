67a4e74a0801529854c2c0648c29a67c3b126b25
tkchieunguyen <bangmuaha123123@gmail.com>
2023-05-13 17:26:58 +0700

    hieu

--- server.js
+++ server.js
@@ -84,12 +84,12 @@ io.on('connection', (socket) => {
     socket.on('C-ReadDigital', (data) => {
         jsonData__ReadDigital = JSON.parse(data)
         console.log(data);
-        console.log(JSON.parse(jsonData__ReadDigital))
+        //console.log(JSON.parse(jsonData__ReadDigital))
 
-        // let led = parseInt(jsonData__ReadDigital.port, 16).toString(2)
-        let led = parseInt(jsonData__ReadDigital, 2)
+        let led = (jsonData__ReadDigital.port).toString(2)
+        //let led = parseInt(jsonData__ReadDigital.port, 2)
         console.log(led)
-        houseId_ReadDigital = parseInt(jsonData__ReadDigital.HouseID, 10)
+        houseId_ReadDigital = parseInt(jsonData__ReadDigital.houseID, 10)
         let bitArray = led.split("")
         jsonLed = '{"bit8":' + bitArray[7] + ',"bit7":' + bitArray[6] + ',"bit6":' + bitArray[5] + ',"bit5":' + bitArray[4] + ',"bit4":' + bitArray[3] + ',"bit3":' + bitArray[2] + '}'
         switch (houseId_ReadDigital) {
@@ -108,13 +108,11 @@ io.on('connection', (socket) => {
     socket.on('C-WriteDigital', (data) => {
         jsonData__ReadDigital = JSON.parse(data)
         console.log(data);
-        //console.log(JSON.parse(jsonData__ReadDigital))
-
-
-        // let led = parseInt(jsonData__ReadDigital.port, 16).toString(2)
-        let led = parseInt(jsonData__ReadDigital, 2)
+        console.log(jsonData__ReadDigital.response);
+        let led = (jsonData__ReadDigital.port).toString(2)
+        //let led = parseInt(jsonData__ReadDigital.port, 2)
         console.log(led)
-        houseId_ReadDigital = parseInt(jsonData__ReadDigital.HouseID, 10)
+        houseId_ReadDigital = parseInt(jsonData__ReadDigital.houseID, 10)
         let bitArray = led.split("")
         jsonLed = '{"bit8":' + bitArray[7] + ',"bit7":' + bitArray[6] + ',"bit6":' + bitArray[5] + ',"bit5":' + bitArray[4] + ',"bit4":' + bitArray[3] + ',"bit3":' + bitArray[2] + '}'
 
@@ -173,7 +171,7 @@ io.on('connection', (socket) => {
     //DOC GIA TRI RS485
     socket.on('C-ReadRS485', (data) => {
         let jsonData = JSON.parse(data)
-        let houseID = jsonData.HouseID
+        let houseID = jsonData.houseID
         //console.log(jsonData)
         //a = { "Client": { "houseID": 1, "response": "RS485", "RS485a": "aRS485 Address", "rs485d1": Data 1, "rs485d2": Data 2, ...} }
         let time = moment().format('YYYY-MM-DD HH:mm:ss')
@@ -227,7 +225,7 @@ io.on('connection', (socket) => {
         switch (jsonData.address) {
             case 35:
                 lightI2C = (parseInt(jsonData.data_1 + jsonData.data_2, 16) / 1.2)
-                switch (jsonData.HouseID) {
+                switch (jsonData.houseID) {
                     case '1':
                         connection.execute('INSERT INTO lighti2c1(light,time) VALUES (?,?)', [lightI2C, time])
                         break
@@ -239,7 +237,7 @@ io.on('connection', (socket) => {
             case 68:
                 temI2C = (parseInt(jsonData.data_1 + jsonData.data_2, 16) * 175 / 65535) - 45
                 humI2C = (parseInt(jsonData.data_4 + jsonData.data_5, 16) * 100 / 65535)
-                switch (jsonData.HouseID) {
+                switch (jsonData.houseID) {
                     case '1':
                         connection.execute('INSERT INTO tem_humi2c1(tem,hum,time) VALUES (?,?,?)', [temI2C, humI2C, time])
                         break
